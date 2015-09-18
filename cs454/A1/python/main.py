fb9cb41dd18ad2eac80929f9d9598dcb943a2b08
Qiyuan Qiu <qqiu@cycle2.csug.rochester.edu>
2015-09-17 23:57:54 -0400

    Make use of level when return
    Instead of using a global counter to figure out how many right
    braces need to be appended.
    
    One can use the level as instead. The number of levels are the
    number of right braces need to be appended.

--- cs454/A1/python/main.py
+++ cs454/A1/python/main.py
@@ -2,18 +2,22 @@ import sys
 
 ans = "("
 
-
 def trees(n_nodes):
+    #define function inside function
     def dfs(level, total):
         global ans
         if total == 0:
-            print( ans + ")"*(2*int(n_nodes)-len(ans)) )
+            #found one answer
+            #print( ans + ")"*(2*int(n_nodes)-len(ans)) )
+            print (ans + ")" * (level+1))
             return
         if level == 0:
+            #at root, have to move deeper
             ans += '('
             dfs(level+1, total-1)
             ans = ans[0:-1]
         else:
+            #have real choice, do one at a time
             ans += '('
             dfs(level+1, total-1)
             ans = ans[0:-1]
@@ -22,6 +26,7 @@ def trees(n_nodes):
             dfs(level-1, total)
             ans = ans[0:-1]
 
+    #start recursion from root
     dfs(0, n_nodes-1)
 
 def main():
