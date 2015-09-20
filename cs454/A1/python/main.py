deda62a4042497a31a6caf79703a3683ebf189fe
Qiyuan Qiu <qqiu@cycle3.csug.rochester.edu>
2015-09-20 16:25:05 -0400

    refactor program

--- cs454/A1/python/main.py
+++ cs454/A1/python/main.py
@@ -1,38 +1,21 @@
 import sys
 
-ans = "("
+def dfs(level, total, ans):
+    if total == 0:
+        print( "(" +  ans + (")"*level) + ")" )
+    elif level == 0:
+        dfs(level+1, total-1, ans + "(")
+    else:
+        dfs(level+1, total-1, ans + "(")
+        dfs(level-1, total, ans + ")")
 
-def trees(n_nodes):
-    #define function inside function
-    def dfs(level, total):
-        global ans
-        if total == 0:
-            #found one answer
-            #print( ans + ")"*(2*int(n_nodes)-len(ans)) )
-            print (ans + ")" * (level+1))
-            return
-        if level == 0:
-            #at root, have to move deeper
-            ans += '('
-            dfs(level+1, total-1)
-            ans = ans[0:-1]
-        else:
-            #have real choice, do one at a time
-            ans += '('
-            dfs(level+1, total-1)
-            ans = ans[0:-1]
-
-            ans += ')'
-            dfs(level-1, total)
-            ans = ans[0:-1]
-
-    #start recursion from root
-    dfs(0, n_nodes-1)
+def trees(n):
+    dfs(0, n-1, "")
 
 def main():
     print ("How many points do you have ?")
-    x = sys.stdin.readline();
-    trees(int(x))
+    x = int(sys.stdin.readline());
+    trees(x)
 
 if __name__ == "__main__":
     main()
