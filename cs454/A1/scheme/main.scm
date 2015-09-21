f2837066973679b84331bfcb1b0e800a7e12e60f
Qiyuan Qiu <qqiu@cycle3.csug.rochester.edu>
2015-09-20 21:02:45 -0400

    use string concatenation

--- cs454/A1/scheme/main.scm
+++ cs454/A1/scheme/main.scm
@@ -1,12 +1,12 @@
 (define (dfs level n str)
   (cond
-    ((= n 0) (display str) (display (make-string (+ level 1) #\))) (newline))
+    ((= n 0) (display (apply string-append (list "(" str (make-string level #\)) ")" ))) (newline))
     ((= level 0) (dfs (+ level 1) (- n 1) (string-append str "(") ))
     (else (dfs (+ level 1) (- n 1) (string-append str "("))
           (dfs (- level 1) n (string-append str ")") ))))
 
 (define (trees n)
-  (dfs 0 (- n 1) "("))
+  (dfs 0 (- n 1) ""))
 
 ;;; Waite for stdin
 (display "How many points do you have?")
