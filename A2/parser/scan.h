ac2dd87d6b0b7954b7a0703d6e5d6f8f6d85fbc4
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-09-24 11:46:01 -0400

    add tokens for if, while, and ro

--- A2/parser/scan.h
+++ A2/parser/scan.h
@@ -1,7 +1,8 @@
 /* definitions the scanner shares with the parser */
 
-typedef enum {t_while, t_if, t_end, t_read, t_write, t_id, t_literal, t_gets,
-                t_add, t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof} token;
+typedef enum {t_equal, t_nequal, t_lt, t_gt, t_le, t_ge,  // ro 
+    t_while, t_if, t_end, t_read, t_write, t_id, t_literal, t_gets, // keywords
+    t_add, t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof} token; // arithmatic operations
 
 extern char token_image[100];
 
