4e3a9e2490e4a3f0848a316914586e973e1dfeb8
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-11-24 21:47:51 -0500

    Print reverse order set

--- A5/extra/src/main.cpp
+++ A5/extra/src/main.cpp
@@ -21,28 +21,28 @@ int main() {
     cout << S[5] << endl;           // should print 0 (false) : 5 not in S
 
     (S += 5) += 7;
-    print(S);                       // should print 3 5 7
+    rprint(S);                       // should print 3 5 7
 
     oset<int> T(3);                      // singleton
-    print(T);              // should print 3
+    rprint(T);              // should print 3
 
     oset<int> U(S);                      // copy of S
     oset<int> V(S);                      // copy of S
 
-    oset<int> W; W = S;  print(W);      // 3 5 7
+    oset<int> W; W = S;  rprint(W);      // 3 5 7
 
     S -= 4;                         // should be a no-op
     S -= 3;
     U -= 5;
     V -= 7;
-    print(S);                       // 5 7
-    print(U);                       // 3 7
-    print(V);                       // 3 5
+    rprint(S);                       // 5 7
+    rprint(U);                       // 3 7
+    rprint(V);                       // 3 5
 
-    oset<int> A;  ((A += 5) += 3) += 4;  print(A);       // 3 4 5
-    oset<int> B;  ((B += 6) += 5) += 7;  print(B);       // 5 6 7
+    oset<int> A;  ((A += 5) += 3) += 4;  rprint(A);       // 3 4 5
+    oset<int> B;  ((B += 6) += 5) += 7;  rprint(B);       // 5 6 7
 
-    oset<int> AuB(A);  AuB += B;  print(AuB);            // 3 4 5 6 7
-    oset<int> AmB(A);  AmB -= B;  print(AmB);            // 3 4
-    oset<int> AiB(A);  AiB *= B;  print(AiB);            // 5
+    oset<int> AuB(A);  AuB += B;  rprint(AuB);            // 3 4 5 6 7
+    oset<int> AmB(A);  AmB -= B;  rprint(AmB);            // 3 4
+    oset<int> AiB(A);  AiB *= B;  rprint(AiB);            // 5
 }
